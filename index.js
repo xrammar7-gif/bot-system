d70e5799b7f4888e9c67a1e8c4cf69a3fcf9183d
xrammar7-gif <xrammar7@gmail.com>
2026-02-28 02:49:02 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client, GatewayIntentBits, Collection, Partials } = require("discord.js");
+const fs = require("fs");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ],
+  partials: [Partials.Channel]
+});
+
+client.commands = new Collection();
+client.prefixCommands = new Collection();
+client.prefix = "$";
+
+// === LOAD HANDLERS ===
+require("./handlers/slashHandler")(client);
+require("./handlers/prefixHandler")(client);
+
+client.once("ready", () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
